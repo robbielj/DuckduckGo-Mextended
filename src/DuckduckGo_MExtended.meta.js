9edac4379f1cd55156b6ffb9cf329736e1617b60
Jguer <joaogg3@gmail.com>
2014-07-26 19:43:11 +0100

    Added meta.js

---
+++ src/DuckduckGo_MExtended.meta.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name            DuckDuckGo MExtended
+// @namespace       greasyfork.org/users/3926-jguer
+// @version         2.1.0
+// @author          jguer
+// @originalauthor  tumpio
+// @description     Extends DDG by adding a customizable list of additional search engines for making fast searches from other engines. Modified version for bigger text,bugfixing and click ease.
+// @icon            https://raw.githubusercontent.com/Jguer/DuckduckGo-Mextended/master/resources/large.png
+// @domain          duckduckgo.com
+// @domain          mycroftproject.com
+// @match           *://duckduckgo.com/*
+// @match           http://mycroftproject.com/*
+// @exclude         *://duckduckgo.com/post2.html
+// @grant           GM_addStyle
+// @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_xmlhttpRequest
+// @updateURL       https://github.com/Jguer/DuckduckGo-Mextended/raw/master/src/DuckduckGo_MExtended.meta.js
+// @downloadURL     https://github.com/Jguer/DuckduckGo-Mextended/raw/master/src/DuckduckGo_MExtended.user.js
+// @updateVersion   2.1.0
+// ==/UserScript==
